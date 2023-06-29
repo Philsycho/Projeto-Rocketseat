02ea9b1f8f7fa9b2f15aeee15bbc2526892c1af9
Philsycho <55813177+Philsycho@users.noreply.github.com>
2023-06-29 10:02:37 -0300

    Configurado para o canal do Phil.

--- script.js
+++ script.js
@@ -8,11 +8,11 @@ function toggleMode() {
   //  substituir a imagem
   if (html.classList.contains("light")) {
     //  se tiver light adicionar/trocar imagem
-    img.setAttribute("src", "./assets/avatar-light.png")
+    img.setAttribute("src", "./assets/Philipinho-2.jpeg")
     img.setAttribute("alt", "Avatar do perfil com oculos escuro")
   } else {
     //  se tiver dark manter a imagem.
-    img.setAttribute("src", "./assets/avatar.png")
+    img.setAttribute("src", "./assets/Philipinho-oculos.png")
     img.setAttribute("alt", "Avatar do perfil sem oculos escuro")
   }
 }
